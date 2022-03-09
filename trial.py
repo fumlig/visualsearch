b92defebc522e5040b7e339e143cbfcba84d81d3
Oskar Lundin <c.oskar.lundin@gmail.com>
2022-03-09 21:47:59 +0100

    some notes in method

--- trial.py
+++ trial.py
@@ -10,23 +10,27 @@ class SignalEnv(gym.Env):
     def __init__(self, signal, window, delta=None):
         self.signal = signal
         self.shape = signal.shape
+        self.ndim = len(self.shape)
         self.window = window
-        self.delta = delta if delta is not None else np.ones(len(self.shape), dtype=int)
+        self.delta = delta if delta is not None else np.ones(self.ndim, dtype=int)
 
-        assert len(self.shape) == len(self.window)
+        assert self.ndim == len(self.window)
+        assert self.ndim == len(self.delta)
         assert all([window_dim <= shape_dim for shape_dim, window_dim in zip(self.shape, self.window)])
 
         self.reward_range = (-np.inf, np.inf)
-        self.action_space = gym.spaces.Discrete(len(self.shape)*2)
+        self.action_space = gym.spaces.Discrete(self.ndim*2)
         self.observation_space = gym.spaces.Box(0, 1, self.window)
 
     def reset(self):
-        self.position = np.zeros(len(self.shape), dtype=int)
+        self.position = np.zeros(self.ndim, dtype=int)
         return self.observation()
 
     def step(self, action):
         dim = action // 2
-        neg = action % 2 == 0   
+        neg = action % 2 == 0
+
+        assert dim < self.ndim
 
         if neg and self.position[dim] - self.delta[dim] >= 0:
             self.position[dim] -= self.delta[dim]
@@ -55,7 +59,7 @@ class SignalEnv(gym.Env):
 if __name__ == "__main__":
     img = cv.imread("data/lenna.png")
 
-    env = SignalEnv(img, (256, 256, 1))
+    env = SignalEnv(img, (256, 256, 1), delta=(32, 32, 2))
     obs = env.reset()
     done = False
 

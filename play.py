ef51ac8633253c5acddd83a15bc22a25ebf9ce7a
Oskar Lundin <oslund@foi.se>
2022-03-16 09:34:36 +0100

    add good video recordings

--- play.py
+++ play.py
@@ -10,116 +10,122 @@ import torch as th
 import gym
 import gym_search
 import random
+import datetime as dt
 
 from gym_search.utils import travel_dist
 
 from agents.ac import ActorCritic
 from agents.random import RandomAgent
 
+
 KEY_ESC = 27
 KEY_RET = 13
 WINDOW_SIZE = (640, 640)
 
 
-parser = ArgumentParser()
-parser.add_argument("env_id", type=str)
-parser.add_argument("--seed", type=int, default=None)
-parser.add_argument("--model", type=str)
-parser.add_argument("--delay", type=int, default=1)
-parser.add_argument("--cpu", action="store_true")
-parser.add_argument("--observe", action="store_true")
-parser.add_argument("--verbose", action="store_true")
-parser.add_argument("--episodes", type=int, default=1024)
-parser.add_argument("--deterministic", action="store_true")
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("env_id", type=str)
+    parser.add_argument("--seed", type=int, default=None)
+    parser.add_argument("--name", type=str, default=dt.datetime.now().isoformat())
+    parser.add_argument("--model", type=str)
+    parser.add_argument("--delay", type=int, default=1)
+    parser.add_argument("--cpu", action="store_true")
+    parser.add_argument("--observe", action="store_true")
+    parser.add_argument("--verbose", action="store_true")
+    parser.add_argument("--episodes", type=int, default=1024)
+    parser.add_argument("--deterministic", action="store_true")
+    parser.add_argument("--record", action="store_true")
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-wrappers = [gym.wrappers.RecordEpisodeStatistics, gym_search.wrappers.ResizeImage, gym_search.wrappers.ObserveOverview]
+    wrappers = [gym.wrappers.RecordEpisodeStatistics, gym_search.wrappers.ResizeImage, gym_search.wrappers.ObserveOverview]
 
-env = gym.make(args.env_id)
-for wrapper in wrappers:
-    env = wrapper(env)
+    env = gym.make(args.env_id)
+    for wrapper in wrappers:
+        env = wrapper(env)
 
-agent = None
-device = th.device("cuda" if th.cuda.is_available() and not args.cpu else "cpu")
-stats = [defaultdict(int) for _ in range(args.episodes)]
+    agent = None
+    device = th.device("cuda" if th.cuda.is_available() and not args.cpu else "cpu")
+    stats = [defaultdict(int) for _ in range(args.episodes)]
 
+    if args.seed is not None:
+        random.seed(args.seed)
+        np.random.seed(args.seed)
+        th.manual_seed(args.seed)
+        env.seed(args.seed)
 
-if args.seed is not None:
-    random.seed(args.seed)
-    np.random.seed(args.seed)
-    th.manual_seed(args.seed)
-    env.seed(args.seed)
+    if args.model:
+        print(f"loading {args.model}")
+        agent = th.load(args.model).to(device)
+        agent.eval()
 
-if args.model:
-    print(f"loading {args.model}")
-    agent = th.load(args.model).to(device)
-    agent.eval()
+    if args.record:
+        env = gym.wrappers.RecordVideo(env, "videos", episode_trigger=lambda _: True)
 
-cv.namedWindow(args.env_id, cv.WINDOW_AUTOSIZE)
+    cv.namedWindow(args.env_id, cv.WINDOW_AUTOSIZE)
 
-for ep in range(args.episodes):
-    
-    done = False
-    obs = env.reset()
+    for ep in range(args.episodes+1):
 
-    if args.verbose:
-        points = [env.view.pos] + [target.pos for target in env.targets]
-        print(points)
-        print("optimal:", travel_dist(points)/env.step_size)
-
-    while not done:
-        if args.observe:
-            img = obs["overview"]*255
-            img = img.astype(dtype=np.uint8)
-            #img = obs["image"]
-        else:
-            img = env.render(mode="rgb_array")
-
-        img = cv.resize(img, WINDOW_SIZE, interpolation=cv.INTER_AREA)
-        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-        cv.imshow(args.env_id, img)	
-
-        key = cv.waitKey(args.delay)
-
-        if agent is None:
-            act = env.get_keys_to_action().get((key,), 0)
-        else:
-            with th.no_grad():
-                obs = {key: th.tensor(sub_obs).float().unsqueeze(0).to(device) for key, sub_obs in obs.items()}
-                act = agent.predict(obs, deterministic=args.deterministic)
-        
-        if args.verbose:
-            step_begin = process_time()
-
-        obs, rew, done, info = env.step(act)
+        done = False
+        obs = env.reset()
 
         if args.verbose:
-            step_end = process_time()
-            print("fps:", 1.0/(step_end - step_begin))
+            points = [env.view.pos] + [target.pos for target in env.targets]
+            print(points)
+            print("optimal:", travel_dist(points)/env.step_size)
 
-        if key == KEY_RET:
-            done = True
+        while not done:
+            if args.observe:
+                #img = obs["overview"]*255
+                #img = img.astype(dtype=np.uint8)
+                img = obs["image"]
+            else:
+                img = env.render(mode="rgb_array")
 
-        if key == KEY_ESC:
-            exit(0)
+            img = cv.resize(img, WINDOW_SIZE, interpolation=cv.INTER_AREA)
+            img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+            cv.imshow(args.env_id, img)	
 
-        stats[ep]["steps"] += 1
-        stats[ep]["return"] += rew
+            key = cv.waitKey(args.delay)
 
-        if args.verbose:
-            print("action:", env.get_action_meanings()[act], "reward:", rew)
+            if agent is None:
+                act = env.get_keys_to_action().get((key,), 0)
+            else:
+                with th.no_grad():
+                    obs = {key: th.tensor(sub_obs).float().unsqueeze(0).to(device) for key, sub_obs in obs.items()}
+                    act = agent.predict(obs, deterministic=args.deterministic)
 
-        if args.observe:
-            #print("observation:", obs)
-            print(obs["overview"].transpose(2, 0, 1))
+            if args.verbose:
+                step_begin = process_time()
 
-        if done:
-            print(", ".join([f"{key}: {value}" for key, value in stats[ep].items()]))
-            #cv.imwrite("search.jpg", env._image(show_path=True))
+            obs, rew, done, info = env.step(act)
 
+            if args.verbose:
+                step_end = process_time()
+                print("fps:", 1.0/(step_end - step_begin))
 
-print(
-    "average return:", sum([ep["return"] for ep in stats])/len(stats),
-    "average length:", sum([ep["length"] for ep in stats])/len(stats),
-)
+            if key == KEY_RET:
+                done = True
+
+            if key == KEY_ESC:
+                exit(0)
+
+            stats[ep]["steps"] += 1
+            stats[ep]["return"] += rew
+
+            if args.verbose:
+                print("action:", env.get_action_meanings()[act], "reward:", rew)
+
+            if args.observe:
+                #print("observation:", obs)
+                print(obs["overview"].transpose(2, 0, 1))
+
+            if done:
+                print(", ".join([f"{key}: {value}" for key, value in stats[ep].items()]))
+                #cv.imwrite("search.jpg", env._image(show_path=True))
+
+    print(
+        "average return:", sum([ep["return"] for ep in stats])/len(stats),
+        "average length:", sum([ep["length"] for ep in stats])/len(stats),
+    )

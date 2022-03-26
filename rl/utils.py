ac2e04dc223adb5542f37bb79645de74305dfa9c
Oskar Lundin <c.oskar.lundin@gmail.com>
2022-03-26 14:41:38 +0100

    add decomposition to image

--- rl/utils.py
+++ rl/utils.py
@@ -3,6 +3,8 @@ import torch as th
 import torch.nn as nn
 import torch.nn.functional as F
 
+from sklearn.decomposition import PCA
+
 
 def init_weights(layer, gain=np.sqrt(2)):
     nn.init.orthogonal_(layer.weight, gain)
@@ -29,3 +31,14 @@ def channels_first(image):
 
 def preprocess_image(image):
     return normalize_image(channels_first(image))
+
+
+def image_decomposition(x):
+    h, w, c = x.shape
+    if c == 3: return x
+    x = x.flatten().reshape(h*w, c)
+    pca = PCA(n_components=3).fit(x)
+    y = pca.transform(x)
+    img = y.reshape(h, w, 3)
+    img *= (255.0/img.max())
+    return np.abs(img)
